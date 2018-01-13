45fda2812ad9229ea999809dc7ae74bb99c570a2
Derek Doty <ddoty.61414@gmail.com>
2018-01-13 10:42:05 -0600

    update

--- public/app.js
+++ public/app.js
@@ -143,7 +143,7 @@ function logout() {
 		localStorage.removeItem('email');
 		$('.loginPage').removeClass('hidden');
 		$('.registerPage').removeClass('hidden');
-		navBar();
+		location.reload();
 	})
 }
 
