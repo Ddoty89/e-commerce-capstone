1e52a9e7487fff48dbcd8c0b0471d171a643bf3d
Derek Doty <ddoty.61414@gmail.com>
2018-05-23 19:45:20 -0400

    config update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongo ds133816.mlab.com:33816/tech-up-capstone -u doty -p 12345';
+    'mongodb://doty:12345@ds133816.mlab.com:33816/tech-up-capstone';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'dominic';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '5d';
